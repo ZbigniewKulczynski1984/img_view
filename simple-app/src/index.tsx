0e31b6e41876a55b8ba6b4df6321c5074d23b161
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-01-27 10:03:11 +0100

    code added

--- simple-app/src/index.tsx
+++ simple-app/src/index.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// react dependencies
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+// hot reload for development
+import { AppContainer } from "react-hot-loader";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import App from "./App";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import "./style.scss";
+
+const root = document.getElementById("root") as HTMLElement;
+
+const render = (Component: React.SFC) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    root,
+  );
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept("./App", () => {
+    render(App);
+  });
+}
