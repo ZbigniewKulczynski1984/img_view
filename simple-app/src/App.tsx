7f98587a91a1eeb6287e00fcf349e1a625b06e92
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-01-27 09:59:50 +0100

    code added

--- simple-app/src/App.tsx
+++ simple-app/src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { BrowserRouter, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Routes from "./Routes";
+
+const reactLogo = require("./assets/React-icon.png");
+
+const App = () => (
+  <BrowserRouter>
+    <main className="container">
+      <div>
+        <h1>hello world!</h1>
+        <img className="container__image" alt="react logo" src={reactLogo} />
+        <p>If you see this everything is working!</p>
+      </div>
+      <ul className="left">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <Routes />
+    </main>
+  </BrowserRouter>
+);
 
 export default App;
